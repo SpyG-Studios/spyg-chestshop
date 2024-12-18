117e3d19d7d7eab7787bfe3ca05fd9e670b7c320
ikoliHU <szilagypet53@gmail.com>
2024-12-18 17:46:32 +0100

    feat: Improve shop interaction handling
    - Refactored owner and buyer logic for better clarity.
    - Added economy transactions for buying from a shop.
    - Enhanced messaging for different scenarios during interactions.

--- src/main/java/com/spygstudios/chestshop/listeners/ShopInteractListener.java
+++ src/main/java/com/spygstudios/chestshop/listeners/ShopInteractListener.java
@@ -1,14 +1,21 @@
 package com.spygstudios.chestshop.listeners;
 
+import java.util.Map;
+
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 
 import com.spygstudios.chestshop.ChestShop;
 import com.spygstudios.chestshop.config.Config;
 import com.spygstudios.chestshop.shop.Shop;
+import com.spygstudios.spyglib.components.ComponentUtils;
+
+import net.milkbowl.vault.economy.EconomyResponse;
 
 public class ShopInteractListener implements Listener {
 
@@ -36,15 +43,41 @@ public class ShopInteractListener implements Listener {
         if (shop == null) {
             return;
         }
-        if (shop.getChestLocation().equals(location) && !shop.getOwner().equals(player.getUniqueId())) {
-            player.sendMessage(config.getMessage("not-owner"));
+
+        // Owner
+        if (shop.getOwner().equals(player.getUniqueId())) {
+            if (shop.getChestLocation().equals(location)) {
+                return;
+            }
+            // shop.openShop(player);
             event.setCancelled(true);
             return;
         }
-        if (shop.getSignLocation().equals(location)) {
-            // shop.openShop(player);
-            event.setCancelled(true);
+
+        // Buyer
+        event.setCancelled(true);
+        if (shop.getChestLocation().equals(location)) {
+            player.sendMessage(config.getMessage("shop.not-owner"));
+            return;
         }
+        if (shop.getMaterial() == null || shop.getAmount() == 0) {
+            player.sendMessage(config.getMessage("shop.setup-needed"));
+            return;
+        }
+
+        EconomyResponse response = ChestShop.getInstance().getEconomy().withdrawPlayer(player, shop.getPrice());
+        if (response.transactionSuccess()) {
+            ChestShop.getInstance().getEconomy().depositPlayer(Bukkit.getOfflinePlayer(shop.getOwner()), shop.getPrice());
+            player.getInventory().addItem(new ItemStack(shop.getMaterial(), shop.getAmount()));
+            player.sendMessage(ComponentUtils.replaceComponent(config.getMessage("shop.bought"), Map.of("%price%", String.valueOf(shop.getPrice()), "%material%", shop.getMaterial().name())));
+            if (shop.doNotify() && Bukkit.getPlayer(shop.getOwner()) != null) {
+                Bukkit.getPlayer(shop.getOwner()).sendMessage(ComponentUtils.replaceComponent(config.getMessage("shop.sold"),
+                        Map.of("%price%", String.valueOf(shop.getPrice()), "%material%", shop.getMaterial().name(), "%player-name%", player.getName())));
+            }
+            return;
+        }
+        player.sendMessage(ComponentUtils.replaceComponent(config.getMessage("not-enough-money"), Map.of("%price%", String.valueOf(shop.getPrice()))));
+
     }
 
 }
