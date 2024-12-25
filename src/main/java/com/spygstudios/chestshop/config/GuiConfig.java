ce99c3b4235b757aaf5b3de960fb8094fa12320b
ikoliHU <szilagypet53@gmail.com>
2024-12-25 17:17:08 +0100

    feat: add new fields for shop material, price, and amount
    Add new fields for shop material, price, and amount in the GUI configuration settings to allow customization of selling items.

--- src/main/java/com/spygstudios/chestshop/config/GuiConfig.java
+++ src/main/java/com/spygstudios/chestshop/config/GuiConfig.java
@@ -15,6 +15,15 @@ public class GuiConfig extends YamlManager {
         set("shop.filleritem.material", "GRAY_STAINED_GLASS_PANE");
         set("shop.filleritem.lore", Arrays.asList("&r"));
 
+        set("shop.material.title", "&cMaterial");
+        set("shop.material.lore", Arrays.asList("&7Put here the material you want to sell in the chest"));
+
+        set("shop.money.title", "&6Price");
+        set("shop.money.lore", Arrays.asList("&7Edit the price you want to sell the material for"));
+
+        set("shop.amount.title", "&6Amount");
+        set("shop.amount.lore", Arrays.asList("&7Edit the amount of items you want to sell"));
+
         set("shop.info.title", "&6&lShop Info");
         set("shop.info.lore", Arrays.asList("&6owner: &7%player-name%", "&6material: &7%material%", "&6sell amount: &7%amount%", "&6price: &7$%price%", "&6created: &7%created%",
                 "&6location: &7%location%", "&6sold items: &7%sold-items%", "&6money earnd: &7$%money-earnd%"));
