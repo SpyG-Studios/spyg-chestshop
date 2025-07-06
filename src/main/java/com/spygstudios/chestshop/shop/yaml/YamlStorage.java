8af74bc0c5f4e542628cbffbb5da7566d02b2a08
ikoli <szilagypet53@gmail.com>
2025-07-06 21:33:18 +0200

    refactor: refactored methods

--- src/main/java/com/spygstudios/chestshop/shop/yaml/YamlStorage.java
+++ src/main/java/com/spygstudios/chestshop/shop/yaml/YamlStorage.java
@@ -27,184 +27,159 @@ public class YamlStorage implements DataManager {
         this.plugin = plugin;
         databaseType = DatabaseType.YAML;
 
-        initialize().thenAccept(success -> {
-            if (success) {
-                plugin.getLogger().info("YamlStorage initialized successfully.");
-            } else {
-                plugin.getLogger().severe("Failed to initialize YamlStorage.");
-            }
-        });
+        boolean initialized = plugin.getDataManager().initialize().join();
+        if (initialized) {
+            plugin.getLogger().info("YamlStorage initialized successfully.");
+        } else {
+            plugin.getLogger().severe("Failed to initialize YamlStorage.");
+        }
     }
 
     @Override
     public CompletableFuture<Shop> createShop(UUID ownerId, String shopName, Location location) {
-        return CompletableFuture.supplyAsync(() -> {
-            String createdAt = plugin.getDataManager().getDateString();
-            Shop shop = new Shop(ownerId, shopName, location, createdAt);
-            YamlShopFile shopFile = YamlShopFile.getShopFile(ownerId);
-            shopFile.addShop(shop);
-            return shop;
-        });
+        String createdAt = plugin.getDataManager().getDateString();
+        Shop shop = new Shop(ownerId, shopName, location, createdAt);
+        YamlShopFile shopFile = YamlShopFile.getShopFile(ownerId);
+        shopFile.addShop(shop);
+        return CompletableFuture.completedFuture(shop);
     }
 
     @Override
     public CompletableFuture<List<Shop>> getPlayerShops(UUID ownerId) {
-        return CompletableFuture.supplyAsync(() -> Shop.getShops(ownerId));
+        List<Shop> shops = Shop.getShops(ownerId);
+        return CompletableFuture.completedFuture(shops);
     }
 
     @Override
-    public CompletableFuture<List<Shop>> getShopsInChunk(Chunk chunk) {
-        return CompletableFuture.supplyAsync(() -> {
-            List<Shop> shops = new ArrayList<>();
-            for (Shop shop : Shop.getShops()) {
-                if (shop.getChestLocation().getWorld() != null && shop.getChestLocation().getWorld().equals(chunk.getWorld())) {
-                    if (shop.getChestLocation().getChunk().equals(chunk)) {
-                        shops.add(shop);
-                    }
+    public CompletableFuture<List<Shop>> loadShopsInChunk(Chunk chunk) {
+        List<Shop> shops = new ArrayList<>();
+        for (Shop shop : Shop.getShops()) {
+            if (shop.getChestLocation().getWorld() != null && shop.getChestLocation().getWorld().equals(chunk.getWorld())) {
+                if (shop.getChestLocation().getChunk().equals(chunk)) {
+                    shops.add(shop);
                 }
             }
-            return shops;
-        });
+        }
+        return CompletableFuture.completedFuture(shops);
     }
 
     @Override
     public CompletableFuture<Shop> getShop(UUID ownerId, String shopName) {
-        return CompletableFuture.supplyAsync(() -> {
-            Shop shop = Shop.getShop(ownerId, shopName);
-            if (shop != null) {
-                return shop;
-            } else {
-                return YamlShopFile.loadShop(plugin, shopName, null, shopName);
-            }
-        });
+        Shop shop = Shop.getShop(ownerId, shopName);
+        if (shop != null) {
+            return CompletableFuture.completedFuture(shop);
+        } else {
+            Shop loadedShop = YamlShopFile.loadShop(plugin, shopName, null, shopName);
+            return CompletableFuture.completedFuture(loadedShop);
+        }
     }
 
     @Override
     public CompletableFuture<Boolean> updateShopPrice(UUID ownerId, String shopName, double price) {
-        return CompletableFuture.supplyAsync(() -> {
-            YamlShopFile shopFile = YamlShopFile.getShopFile(ownerId);
-            shopFile.setPrice(shopName, price);
-            return true;
-        });
+        YamlShopFile shopFile = YamlShopFile.getShopFile(ownerId);
+        shopFile.setPrice(shopName, price);
+        return CompletableFuture.completedFuture(true);
     }
 
     @Override
     public CompletableFuture<Boolean> updateShopMaterial(UUID ownerId, String shopName, Material material) {
-        return CompletableFuture.supplyAsync(() -> {
-            YamlShopFile shopFile = YamlShopFile.getShopFile(ownerId);
-            shopFile.setMaterial(shopName, material);
-            return true;
-        });
+        YamlShopFile shopFile = YamlShopFile.getShopFile(ownerId);
+        shopFile.setMaterial(shopName, material);
+        return CompletableFuture.completedFuture(true);
     }
 
     @Override
     public CompletableFuture<Boolean> updateShopNotify(UUID ownerId, String shopName, boolean notify) {
-        return CompletableFuture.supplyAsync(() -> {
-            YamlShopFile shopFile = YamlShopFile.getShopFile(ownerId);
-            shopFile.overwriteSet("shops." + shopName + ".do-notify", notify);
-            shopFile.markUnsaved();
-            return true;
-        });
+        YamlShopFile shopFile = YamlShopFile.getShopFile(ownerId);
+        shopFile.overwriteSet("shops." + shopName + ".do-notify", notify);
+        shopFile.markUnsaved();
+        return CompletableFuture.completedFuture(true);
     }
 
     @Override
     public CompletableFuture<Boolean> updateSoldItems(UUID ownerId, String shopName, int soldItems) {
-        return CompletableFuture.supplyAsync(() -> {
-            YamlShopFile shopFile = YamlShopFile.getShopFile(ownerId);
-            String shopPath = "shops." + shopName;
-            shopFile.overwriteSet(shopPath + ".sold-items", shopFile.getInt(shopPath + ".sold-items") + soldItems);
-            shopFile.markUnsaved();
-            return true;
-        });
+        YamlShopFile shopFile = YamlShopFile.getShopFile(ownerId);
+        String shopPath = "shops." + shopName;
+        shopFile.overwriteSet(shopPath + ".sold-items", shopFile.getInt(shopPath + ".sold-items") + soldItems);
+        shopFile.markUnsaved();
+        return CompletableFuture.completedFuture(true);
     }
 
     @Override
     public CompletableFuture<Boolean> updateMoneyEarned(UUID ownerId, String shopName, double moneyEarned) {
-        return CompletableFuture.supplyAsync(() -> {
-            YamlShopFile shopFile = YamlShopFile.getShopFile(ownerId);
-            String shopPath = "shops." + shopName;
-            shopFile.overwriteSet(shopPath + ".money-earned", shopFile.getDouble(shopPath + ".money-earned") + moneyEarned);
-            shopFile.markUnsaved();
-            return true;
-        });
+        YamlShopFile shopFile = YamlShopFile.getShopFile(ownerId);
+        String shopPath = "shops." + shopName;
+        shopFile.overwriteSet(shopPath + ".money-earned", shopFile.getDouble(shopPath + ".money-earned") + moneyEarned);
+        shopFile.markUnsaved();
+        return CompletableFuture.completedFuture(true);
     }
 
     @Override
     public CompletableFuture<Boolean> updateShopStats(UUID ownerId, String shopName, int soldItems, double moneyEarned) {
         return updateMoneyEarned(ownerId, shopName, moneyEarned)
-                .thenCompose(result1 -> updateSoldItems(ownerId, shopName, soldItems))
-                .thenApply(result2 -> true);
+                .thenCompose(moneyUpdated -> updateSoldItems(ownerId, shopName, soldItems))
+                .thenApply(soldItemsUpdated -> true);
     }
 
     @Override
     public CompletableFuture<Boolean> renameShop(UUID ownerId, String oldName, String newName) {
-        return CompletableFuture.supplyAsync(() -> {
-            YamlShopFile shopFile = YamlShopFile.getShopFile(ownerId);
-            shopFile.renameShop(oldName, newName);
-            return true;
-        });
+        YamlShopFile shopFile = YamlShopFile.getShopFile(ownerId);
+        shopFile.renameShop(oldName, newName);
+        return CompletableFuture.completedFuture(true);
     }
 
     @Override
     public CompletableFuture<Boolean> deleteShop(UUID ownerId, String shopName) {
-        return CompletableFuture.supplyAsync(() -> {
-            YamlShopFile.getShopFile(ownerId).removeShop(shopName);
-            return true;
-        });
+        YamlShopFile.getShopFile(ownerId).removeShop(shopName);
+        return CompletableFuture.completedFuture(true);
     }
 
     @Override
     public CompletableFuture<List<UUID>> getShopPlayers(UUID ownerId, String shopName) {
-        return CompletableFuture.supplyAsync(() -> {
-            YamlShopFile shopFile = YamlShopFile.getShopFile(ownerId);
-            List<UUID> players = shopFile.getAddedUuids(shopName);
-            if (players != null) {
-                return players;
-            } else {
-                Bukkit.getLogger().warning("No players found for shop: " + shopName);
-                return List.of();
-            }
-        });
+        YamlShopFile shopFile = YamlShopFile.getShopFile(ownerId);
+        List<UUID> players = shopFile.getAddedUuids(shopName);
+        if (players != null) {
+            return CompletableFuture.completedFuture(players);
+        } else {
+            Bukkit.getLogger().warning("No players found for shop: " + shopName);
+            return CompletableFuture.completedFuture(List.of());
+        }
     }
 
     @Override
     public CompletableFuture<Boolean> addPlayerToShop(UUID ownerId, String shopName, UUID toAdd) {
-        return getShop(ownerId, shopName).thenApply(shop -> {
-            if (shop == null) {
-                Bukkit.getLogger().warning("Shop not found with while adding player. Shopname: " + shopName);
-                return false;
-            }
+        Shop shop = getShop(ownerId, shopName).join();
+        if (shop == null) {
+            Bukkit.getLogger().warning("Shop not found with while adding player. Shopname: " + shopName);
+            return CompletableFuture.completedFuture(false);
+        }
 
-            YamlShopFile shopFile = YamlShopFile.getShopFile(shop.getOwnerId());
-            shopFile.addPlayer(toAdd, shop.getName());
-            return true;
-        });
+        YamlShopFile shopFile = YamlShopFile.getShopFile(shop.getOwnerId());
+        shopFile.addPlayer(toAdd, shop.getName());
+        return CompletableFuture.completedFuture(true);
     }
 
     @Override
     public CompletableFuture<Boolean> removePlayerFromShop(UUID ownerId, String shopName, UUID toRemove) {
-        return getShop(ownerId, shopName).thenApply(shop -> {
-            if (shop == null) {
-                Bukkit.getLogger().warning("Shop not found for owner: " + ownerId + ", shop name: " + shopName);
-                return false;
-            }
+        Shop shop = getShop(ownerId, shopName).join();
+        if (shop == null) {
+            Bukkit.getLogger().warning("Shop not found for owner: " + ownerId + ", shop name: " + shopName);
+            return CompletableFuture.completedFuture(false);
+        }
 
-            YamlShopFile shopFile = YamlShopFile.getShopFile(ownerId);
-            shopFile.removePlayer(toRemove, shopName);
-            return true;
-        });
+        YamlShopFile shopFile = YamlShopFile.getShopFile(ownerId);
+        shopFile.removePlayer(toRemove, shopName);
+        return CompletableFuture.completedFuture(true);
     }
 
     @Override
     public CompletableFuture<Boolean> initialize() {
-        return CompletableFuture.supplyAsync(() -> {
-            try {
-                YamlShopFile.loadShopFiles(plugin);
-                return true;
-            } catch (Exception e) {
-                return false;
-            }
-        });
+        try {
+            YamlShopFile.loadShopFiles(plugin);
+            return CompletableFuture.completedFuture(true);
+        } catch (Exception e) {
+            return CompletableFuture.completedFuture(false);
+        }
     }
 
     public void close() {
@@ -213,15 +188,13 @@ public class YamlStorage implements DataManager {
 
     @Override
     public CompletableFuture<Boolean> saveShop(Shop shop) {
-        return CompletableFuture.supplyAsync(() -> {
-            YamlShopFile shopFile = YamlShopFile.getShopFile(shop.getOwnerId());
-            if (shopFile == null) {
-                return false;
-            }
+        YamlShopFile shopFile = YamlShopFile.getShopFile(shop.getOwnerId());
+        if (shopFile == null) {
+            return CompletableFuture.completedFuture(false);
+        }
 
-            YamlShopFile.saveShopFile(shopFile);
-            return true;
-        });
+        YamlShopFile.saveShopFile(shopFile);
+        return CompletableFuture.completedFuture(true);
     }
 
     @Override
