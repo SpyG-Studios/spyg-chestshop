90e10ef87fca59a32eea830e3c62e2618f0f906c
ikoli <szilagypet53@gmail.com>
2026-02-25 20:37:38 +0100

    fix: add balance check before withdrawing for purchases and sales

--- src/main/java/com/spygstudios/chestshop/shop/ShopTransactions.java
+++ src/main/java/com/spygstudios/chestshop/shop/ShopTransactions.java
@@ -39,10 +39,16 @@ public class ShopTransactions {
 
         double itemsPrice = itemCount * shop.getCustomerPurchasePrice();
         Economy economy = plugin.getEconomy();
+
+        if (!economy.has(buyer, itemsPrice)) {
+            Message.NOT_ENOUGH_MONEY.send(buyer, Map.of("%price%", String.valueOf(itemsPrice)));
+            return;
+        }
+
         EconomyResponse response = economy.withdrawPlayer(buyer, itemsPrice);
 
         if (!response.transactionSuccess()) {
-            Message.NOT_ENOUGH_MONEY.send(buyer, Map.of("%price%", String.valueOf(itemsPrice)));
+
             return;
         }
 
@@ -104,9 +110,8 @@ public class ShopTransactions {
 
         double itemsPrice = amount * shop.getCustomerSalePrice();
         Economy economy = plugin.getEconomy();
-        EconomyResponse response = economy.withdrawPlayer(Bukkit.getOfflinePlayer(shop.getOwnerId()), itemsPrice);
 
-        if (!response.transactionSuccess()) {
+        if (!economy.has(Bukkit.getOfflinePlayer(shop.getOwnerId()), itemsPrice)) {
             Message.SHOP_OWNER_NO_MONEY.send(seller);
             if (shop.isNotify()) {
                 Player owner = Bukkit.getPlayer(shop.getOwnerId());
@@ -114,6 +119,11 @@ public class ShopTransactions {
                     Message.SHOP_OWNER_NO_MONEY_OWNER.send(owner, Map.of("%player-name%", seller.getName(), "%item%", shop.getName(), "%price%", String.valueOf(itemsPrice)));
                 }
             }
+            return;
+        }
+        EconomyResponse response = economy.withdrawPlayer(Bukkit.getOfflinePlayer(shop.getOwnerId()), itemsPrice);
+        if (!response.transactionSuccess()) {
+            plugin.getLogger().warning("Failed to withdraw money from shop owner " + shop.getOwnerId() + " for shop " + shop.getName() + ": " + response.errorMessage);
             return;
         }
 
